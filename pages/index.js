fe420ac0fbfa032a0c42a4348baa24232fca0921
Daniel <daniel.russi@hotmail.com>
2022-07-05 15:01:28 -0500

    second commit (commit main)

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,6 @@
 import Head from "next/head";
 import { Typography } from "@mui/material";
+import Layout from "../components/Layout";
 export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>Sanity amazona</title>
-        <meta
-          name="description"
-          content="The eccomerce website by next and sanity"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Typography component="h1" variant="h1">
-        Sanity Amazona
-      </Typography>
-    </div>
-  );
+  return <Layout>List Products</Layout>;
 }
